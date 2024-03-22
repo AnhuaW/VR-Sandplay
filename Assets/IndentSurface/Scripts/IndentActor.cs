eb2ab4d62b7b5167a7e1ca60caf14b7e5d7fc347
Wu <anhuawu@UMICH.EDU>
2024-03-22 17:08:57 -0400

    raycast edit

--- Assets/IndentSurface/Scripts/IndentActor.cs
+++ Assets/IndentSurface/Scripts/IndentActor.cs
@@ -1,42 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Wacki.IndentSurface;
 
-
-namespace Wacki.IndentSurface
+/// <summary>
+/// Simple control script for our sphere that leaves a track in the snow.
+/// </summary>
+public class IndentActor : MonoBehaviour
 {
-    /// <summary>
-    /// Simple control script for our sphere that leaves a track in the snow.
-    /// </summary>
-    public class IndentActor : MonoBehaviour
-    {
-        [Range(0.0f, 0.2f)]
-        public float drawDelta = 0.01f;
-        private Vector3 _prevDrawPos;
+    [Range(0.0f, 0.2f)]
+    // DrawDelta
+    public float drawDelta = 0.01f;
+    private Vector3 _prevDrawPos;
         
-        void Update()
+    void Update()
+    {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+
+        GetComponent<Rigidbody>().AddTorque(v, 0, -h);
+
+        if (Vector3.Distance(_prevDrawPos, transform.position) < drawDelta)
+            return;
+
+        _prevDrawPos = transform.position;
+
+        Debug.DrawLine(transform.position, transform.position + Vector3.down);
+
+        //check raycast distance before creatng indentation
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit) && hit.distance < 0.1f)
         {
-            float v = Input.GetAxis("Vertical");
-            float h = Input.GetAxis("Horizontal");
-
-            GetComponent<Rigidbody>().AddTorque(v, 0, -h);
-
-            if (Vector3.Distance(_prevDrawPos, transform.position) < drawDelta)
+            var texDraw = hit.collider.gameObject.GetComponent<IndentDraw>();
+            if (texDraw == null)
                 return;
 
-            _prevDrawPos = transform.position;
-
-            Debug.DrawLine(transform.position, transform.position + Vector3.down);
-
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, Vector3.down, out hit))
-            {
-                var texDraw = hit.collider.gameObject.GetComponent<IndentDraw>();
-                if (texDraw == null)
-                    return;
-
-                texDraw.IndentAt(hit);
-            }
+            texDraw.IndentAt(hit);
         }
     }
-
 }
